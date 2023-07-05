a6b3ec3bebba66058661348fb1fdc173710dead6
Helltraitor <helltraitor@hotmail.com>
2023-07-05 20:21:42 +0300

    Added: @unocss/nuxt and sass for playground styling

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,4 +1,18 @@
+import {
+  presetAttributify as PresetAttributify,
+  presetUno as PresetUno,
+} from 'unocss'
+
 export default defineNuxtConfig({
-  modules: ['../src/module'],
-  devtools: { enabled: true }
+  modules: [
+    '../src/module',
+    '@unocss/nuxt',
+  ],
+  devtools: { enabled: true },
+  unocss: {
+    presets: [
+      PresetAttributify(),
+      PresetUno(),
+    ],
+  },
 })
